62b9da333717e3f716dddf3feece895bc7a1259b
Astariul <remondnicola@gmail.com>
2023-04-28 00:32:57 +0900

    🔥 Remove dummy code

--- tests/test_clothion.py
+++ tests/test_clothion.py
@@ -1,13 +1,5 @@
-import pytest
-
-from clothion import is_odd
+import clothion
 
 
-@pytest.mark.parametrize("x", [1, 3, 7, 348973243, -1])
-def test_with_odd_numbers(x):
-    assert is_odd(x)
-
-
-@pytest.mark.parametrize("x", [0, 2, 10, 74353434, -8])
-def test_with_even_numbers(x):
-    assert not is_odd(x)
+def test_package_has_version():
+    assert len(clothion.__version__) > 0
