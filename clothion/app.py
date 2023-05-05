32913640b83fdb722c12a22a1fa756cc042a76d3
Astariul <remondnicola@gmail.com>
2023-05-05 15:55:56 +0900

    ✨ Add action + redirection when form is submitted

--- clothion/app.py
+++ clothion/app.py
@@ -1,9 +1,9 @@
 import pathlib
-from typing import List
+from typing import Annotated, List
 
 import uvicorn
-from fastapi import Depends, FastAPI, HTTPException, Request
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi import Depends, FastAPI, Form, HTTPException, Request
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
@@ -26,7 +26,7 @@ def get_db():
 
 @app.get("/", response_class=HTMLResponse)
 async def welcome(request: Request):
-    return templates.TemplateResponse("welcome.html", {"request": request, "id": 3})
+    return templates.TemplateResponse("welcome.html", {"request": request})
 
 
 @app.get("/favicon.ico", include_in_schema=False)
@@ -37,6 +37,33 @@ async def favicon():
 @app.get("/version", tags=["API"])
 async def version() -> str:
     return __version__
+
+
+@app.post("/create")
+def create(integration: Annotated[str, Form()], table: Annotated[str, Form()], db: Session = Depends(get_db)) -> int:
+    db_integration = crud.get_integration_by_token(db, token=integration)
+    if not db_integration:
+        # If the Integration doesn't exist, create it and create the table directly
+        # because if there was no integration, there was no table !
+        db_integration = crud.create_integration(db=db, token=integration)
+        db_table = crud.create_table(db=db, integration_id=db_integration.id, table_id=table)
+    else:
+        # If the Integration exists, we have to check if the table already exists or not
+        db_table = crud.get_table_by_table_id(db, integration_id=db_integration.id, table_id=table)
+        if not db_table:
+            db_table = crud.create_table(db=db, integration_id=db_integration.id, table_id=table)
+
+    return RedirectResponse(f"/{db_integration.id}/{db_table.id}", status_code=301)
+
+
+@app.get("/{integration_id}/{table_id}", response_class=HTMLResponse)
+def widget(request: Request, integration_id: int, table_id: int):
+    return templates.TemplateResponse(
+        "widget.html", {"request": request, "integration_id": integration_id, "table_id": table_id}
+    )
+
+
+#  ################ TODO : clean the following routes
 
 
 @app.post("/integration", response_model=schemas.Integration)
--- clothion/database/crud.py
+++ clothion/database/crud.py
@@ -3,7 +3,7 @@ from typing import Callable
 
 from sqlalchemy.orm import Session
 
-from clothion.database import engine, models, schemas
+from clothion.database import engine, models
 
 
 def create_tables():
@@ -50,18 +50,18 @@ def generate_random_unique_id(uniq_fn: Callable[int, bool]) -> int:
     return random_id
 
 
-def create_integration(db: Session, integration: schemas.IntegrationCreate):
+def create_integration(db: Session, token: str):
     # Create a random ID that doesn't exist on the table yet
     random_id = generate_random_unique_id(lambda i: get_integration(db=db, id=i) is None)
 
-    db_integration = models.Integration(id=random_id, token=integration.token)
+    db_integration = models.Integration(id=random_id, token=token)
     db.add(db_integration)
     db.commit()
     db.refresh(db_integration)
     return db_integration
 
 
-def get_table_by_table_id(db: Session, integration_id: int, table_id: int):
+def get_table_by_table_id(db: Session, integration_id: int, table_id: str):
     return (
         db.query(models.Table)
         .filter(models.Table.integration_id == integration_id)
@@ -83,11 +83,11 @@ def get_tables_of(db: Session, integration_id: int, skip: int = 0, limit: int = 
     return db.query(models.Table).filter(models.Table.integration_id == integration_id).offset(skip).limit(limit).all()
 
 
-def create_table(db: Session, table: schemas.TableCreate, integration_id: int):
+def create_table(db: Session, integration_id: int, table_id: str):
     # Create a random ID that doesn't exist on the table yet
     random_id = generate_random_unique_id(lambda i: get_table(db=db, integration_id=integration_id, id=i) is None)
 
-    db_table = models.Table(id=random_id, **table.dict(), integration_id=integration_id)
+    db_table = models.Table(id=random_id, table_id=table_id, integration_id=integration_id)
     db.add(db_table)
     db.commit()
     db.refresh(db_table)
