534de82cae6bb521903c5694002a26ffca7c5b1d
Astariul <remondnicola@gmail.com>
2023-05-24 20:44:48 +0900

    ✨ Add basic test for filter function

--- tests/test_clothion.py
+++ tests/test_clothion.py
@@ -494,3 +494,23 @@ def test_calculate_data_unique_count(client):
 
     assert len(data) == 1
     assert {"my_title": 5, "email": 5, "price": 4, "day_of": 2, "ckbox": 2, "choices": 4} in data
+
+
+def test_filter_data_boolean_is_basic(client):
+    integration_id, table_id = create_table(client, "secret_token", "table_for_general_data")
+
+    # Get values that are True
+    response = client.post(f"/{integration_id}/{table_id}/data", json={"filter": {"ckbox": {"is": True}}})
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data) == 2
+    assert all(x["ckbox"] is True for x in data)
+
+    # Get values that are False
+    response = client.post(f"/{integration_id}/{table_id}/data", json={"filter": {"ckbox": {"is": False}}})
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data) == 6
+    assert all(x["ckbox"] is False for x in data)
