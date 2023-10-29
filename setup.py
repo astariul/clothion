0e47603aa849ed2c3b466a5ff1ef2b18332c4a2f
Astariul <remondnicola@gmail.com>
2023-10-29 22:27:51 +0100

    ⬆️ Fix version of Ruff

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ reqs = [
 extras_require = {
     "admin": ["alembic~=1.10"],
     "test": ["pytest~=7.0", "pytest-cov~=4.1", "coverage-badge~=1.0"],
-    "lint": ["black~=23.3", "ruff~=0.1.2", "pre-commit~=3.2"],
+    "lint": ["black~=23.3", "ruff~=0.1", "pre-commit~=3.2"],
 }
 extras_require["all"] = sum(extras_require.values(), [])
 extras_require["dev"] = extras_require["test"] + extras_require["lint"]
